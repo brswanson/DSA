236e3c1ba01acc0135c765bc7cd52875f5930545
Ben Swanson <b.swanson@kunzleigh.com>
2018-03-26 15:04:59 -0400

    Adding NthDigit problem definition.

---
+++ DSA/NthDigit.cs
@@ -0,0 +1,18 @@
+﻿namespace DSA
+{
+    /// <summary>
+    ///     Find the nth digit of the infinite integer sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...
+    /// </summary>
+    /// <input>
+    ///     11
+    /// </input>
+    /// <output>
+    ///     0
+    /// </output>
+    /// <notes>
+    ///     n is positive and will fit within the range of a 32-bit signed integer (n less than 231).
+    /// </notes>
+    internal class NthDigit
+    {
+    }
+}
