05bec58a4464db354b3c86aedbd76c9800cc72ac
Ben Swanson <b.swanson@kunzleigh.com>
2018-03-26 15:03:15 -0400

    Adding ToeplitzMatrix proglem definition.

---
+++ DSA/ToeplitzMatrix.cs
@@ -0,0 +1,27 @@
+﻿namespace DSA
+{
+    /// <summary>
+    ///     Verify if a given matrix is a Toeplitz matrix.
+    ///     Follow up:
+    ///     Assume that the whole matrix cannot be fit in memory and should be read from a file.
+    ///     Assume that a few rows and all columns can be read in.
+    ///     How would you then verify the input is a Toeplitz matrix?
+    /// </summary>
+    /// <input>
+    ///     value[]
+    ///     a, b, c, d, e
+    ///     f, a, b, c, d
+    ///     g, f, a, b, c
+    ///     h, g, f, a, b
+    ///     i, h, g, f, a
+    /// </input>
+    /// <output>
+    ///     true
+    /// </output>
+    /// <notes>
+    ///     In linear algebra, a Toeplitz matrix is a matrix in which each descending diagonal from left to right is constant.
+    /// </notes>
+    internal class ToeplitzMatrix
+    {
+    }
+}
