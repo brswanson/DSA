c87a7486c11fa4d052129e6f923cf08f5c465b52
Ben Swanson <b.swanson@kunzleigh.com>
2018-03-26 14:21:18 -0400

    Added 2d matrix sum problem definition.

---
+++ DSA/2DMatrixSum.cs
@@ -0,0 +1,22 @@
+﻿namespace DSA
+{
+    internal class _2DMatrixSum
+    {
+        /// <summary>
+        ///     Given a 2D matrix M X N, support two operations:
+        ///         1. Query(row1, col1, row2, col2) such that I get the sum of all numbers in the rectangle((row1, col1), (row1, col2), (row2, col1), (row2, col2))
+        ///         2. Update(row, col) to a new number
+        ///     Query is a very frequent operation and update is a rare operation, so query should be really fast, but update can be slower.
+        /// </summary>
+        /// <input>
+        ///     int 1, int 1, int 2, int 2
+        ///     Example arrayarray[3][3]
+        ///     1   2   3
+        ///     4   5   6
+        ///     7   8   9
+        /// </input>
+        /// <output>
+        ///     28
+        /// </output>
+    }
+}
