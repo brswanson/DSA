5ef3821bf29104c052346bdb199d6d9dcfe86ea5
Ben Swanson <b.swanson@kunzleigh.com>
2018-03-29 09:38:34 -0400

    Added a solution for in place insertion sort.

--- DSA/Sorting/InsertionSort.cs
+++ DSA/Sorting/InsertionSort.cs
@@ -5,10 +5,31 @@ namespace DSA.Sorting
     [TestClass]
     public class InsertionSort
     {
-        public static int[] SortIntegerArrayAscending(int[] input)
+        public static void SortIntegerArrayAscending(int[] input)
         {
-            // TODO: Actually implement this function
-            return input;
+            // Time:    O(n2). Iterates over each integer in the array at least once (n). May also iterate over each integer in the array once for each integer in the array (n).
+            // Memory:  O(1). Sorts in place.
+
+            // Checking edge cases
+            if (input.Length <= 1) return;
+
+            var index = 1;
+
+            while (index < input.Length)
+            {
+                var innerIndex = index;
+
+                while (innerIndex > 0 && input[innerIndex - 1] > input[innerIndex])
+                {
+                    var placeHolder = input[innerIndex];
+                    input[innerIndex] = input[innerIndex - 1];
+                    input[innerIndex - 1] = placeHolder;
+
+                    innerIndex--;
+                }
+
+                index++;
+            }
         }
 
         [TestMethod]
@@ -17,12 +38,12 @@ namespace DSA.Sorting
             var input = new[] { 7, 3, 1, 9, 8, 2, 4, 6, 5, 0 };
             var expected = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-            var actual = SortIntegerArrayAscending(input);
+            SortIntegerArrayAscending(input);
 
-            Assert.AreEqual(expected.Length, actual.Length);
+            Assert.AreEqual(expected.Length, input.Length);
             for (var i = 0; i < expected.Length; i++)
             {
-                Assert.AreEqual(expected[i], actual[i]);
+                Assert.AreEqual(expected[i], input[i]);
             }
         }
     }
