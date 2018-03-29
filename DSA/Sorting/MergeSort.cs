190242e8acce25cc481b405f99b8794f6b533dbc
Ben Swanson <b.swanson@kunzleigh.com>
2018-03-29 10:42:55 -0400

    Finished implementing merge sort.

--- DSA/Sorting/MergeSort.cs
+++ DSA/Sorting/MergeSort.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace DSA.Sorting
 {
@@ -11,10 +12,66 @@ namespace DSA.Sorting
             // Memory: 2n => n. Passes n, then n/2, n/4, etc ... In each subsequent call. This totals to at most 2n which reduces down to n.
 
             // Checking edge cases
-            if (input == null || input.Length <= 1) return input;
+            if (input == null) return null;
 
-            // TODO: Implement the sort
-            return input;
+            return MergeSortRecursive(input);
+        }
+
+        private static int[] MergeSortRecursive(int[] input)
+        {
+            if (input.Length <= 1) return input;
+
+            var mid = input.Length / 2;
+            // Trade off here of using Skip/Take instead of Array.Copy()
+            var arrayA = input.Take(mid).ToArray();
+            var arrayB = input.Skip(mid).Take(input.Length - mid).ToArray();
+
+            return Merge(MergeSortRecursive(arrayA), MergeSortRecursive(arrayB));
+        }
+
+        private static int[] Merge(int[] arrayA, int[] arrayB)
+        {
+            var mergedArray = new int[arrayA.Length + arrayB.Length];
+            var mergedIndex = 0;
+            var indexA = 0;
+            var indexB = 0;
+
+            // Merge A & B until one is finished
+            while (indexA < arrayA.Length && indexB < arrayB.Length)
+            {
+                if (arrayA[indexA] > arrayB[indexB])
+                {
+                    mergedArray[mergedIndex] = arrayB[indexB];
+                    indexB++;
+                }
+                else
+                {
+                    mergedArray[mergedIndex] = arrayA[indexA];
+                    indexA++;
+                }
+
+                mergedIndex++;
+            }
+
+            // Merge the remainer of A, if any
+            while (indexA < arrayA.Length)
+            {
+                mergedArray[mergedIndex] = arrayA[indexA];
+
+                indexA++;
+                mergedIndex++;
+            }
+
+            // Merge the remainer of B, if any
+            while (indexB < arrayB.Length)
+            {
+                mergedArray[mergedIndex] = arrayB[indexB];
+
+                indexB++;
+                mergedIndex++;
+            }
+
+            return mergedArray;
         }
 
         [TestMethod]
