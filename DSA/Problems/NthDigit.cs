e92808dd965d24ad7c976b90d51ec7e3590be083
Ben Swanson <swansob@gmail.com>
2018-04-02 23:19:26 -0400

    Added a simple brute force solution for NthDigit.

--- DSA/Problems/NthDigit.cs
+++ DSA/Problems/NthDigit.cs
@@ -1,18 +1,56 @@
-﻿namespace DSA.Problems
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DSA.Problems
 {
-    /// <summary>
-    ///     Find the nth digit of the infinite integer sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...
-    /// </summary>
-    /// <input>
-    ///     11
-    /// </input>
-    /// <output>
-    ///     0
-    /// </output>
-    /// <notes>
-    ///     n is positive and will fit within the range of a 32-bit signed integer (n less than 231).
-    /// </notes>
-    internal class NthDigit
+    [TestClass]
+    public class NthDigit
     {
+        /// <summary>
+        ///     Find the nth digit of the positive infinite integer sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...
+        /// </summary>
+        /// <input>
+        ///     11
+        /// </input>
+        /// <output>
+        ///     0
+        /// </output>
+        /// <notes>
+        ///     n is positive and will fit within the range of a 32-bit signed integer (n less than 231).
+        /// </notes>
+        public int BruteForce(int input)
+        {
+            // Time:    O(n). Iterates over integers in the sequence until it reaches the desired length.
+            // Memory:  O(1). Requires a constant amount of additonal memory to keep track of its place in the integer sequence.
+
+            // Safe to return early here since integers must be positive
+            if (input <= 9) return input;
+
+            var digitIndex = 10;
+            var currentNumber = 10;
+
+            while (true)
+            {
+                var numberToString = currentNumber.ToString();
+                foreach (var c in numberToString)
+                {
+                    digitIndex++;
+
+                    if (digitIndex > input) return Convert.ToInt32(char.GetNumericValue(c));
+                }
+
+                currentNumber++;
+            }
+        }
+
+        [TestMethod]
+        public void TestNthDigit()
+        {
+            const int input = 11;
+            const int expected = 0;
+            var actual = BruteForce(input);
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
