3c405b72c1cd770b52a991f23218a7cb79308029
Ben Swanson <b.swanson@kunzleigh.com>
2018-04-06 10:36:27 -0400

    Refactored in some constants and tweaked the input to be easier to parse.

--- DSA/Problems/CourseSchedule.cs
+++ DSA/Problems/CourseSchedule.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace DSA.Problems
 {
@@ -23,24 +24,27 @@ namespace DSA.Problems
         ///     To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1.
         ///     So it is impossible.
         /// </notes>
-        public bool AdjacencyMatrix(int numCourses, int[,] prerequisites)
+        public bool AdjacencyMatrix(int numCourses, List<int[]> prerequisites)
         {
             // Time:    O(n).   One pass over each edge.
             // Memory:  O(n^2). One value for each edge combination. Edges are from 0 to n, so the matrix must be nxn in size.
 
             // Checking edge cases
-            if (numCourses <= 0 || prerequisites.Length <= 0) return true;
+            if (numCourses <= 0 || prerequisites.Count <= 0) return true;
 
+            const int courseIndex = 0;
+            const int prereqIndex = 1;
             var adjMatrix = new bool[numCourses, numCourses];
 
-            // Note: Incomplete. The adj matrix has been created, but the search still needs to exist.
-            //       Also may want to keep track of incoming edges.
-            for (var i = 0; i < prerequisites.GetLength(0) - 1; i++)
+            for (var i = 0; i < prerequisites.Count; i++)
             {
-                var edgeA = prerequisites[i, 0];
-                var edgeB = prerequisites[i, 1];
+                var edgeA = prerequisites[i][courseIndex];
+                var edgeB = prerequisites[i][prereqIndex];
 
                 adjMatrix[edgeA, edgeB] = true;
+
+                // If the opposite incoming/outgoing edge also exists, we know we have a prereq loop
+                if (adjMatrix[edgeB, edgeA]) return false;
             }
 
             return true;
@@ -49,10 +53,13 @@ namespace DSA.Problems
         [TestMethod]
         public void TestCourseSchedule()
         {
-            var input = new[,] { { 1, 0 }, { 0, 1 } };
+            var input = new List<int[]>();
+            input.Add(new[] { 1, 0 });
+            input.Add(new[] { 0, 1 });
+
             const bool expected = false;
 
-            var actual = AdjacencyMatrix(2, input);
+            var actual = AdjacencyMatrix(input.Count, input);
 
             Assert.AreEqual(expected, actual);
         }
