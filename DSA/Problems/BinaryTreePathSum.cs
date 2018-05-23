e068cba9effdb657e7af8652877c5aa5c0f4d3e4
Ben Swanson <b.swanson@kunzleigh.com>
2018-05-23 09:33:47 -0400

    Added more definition to the BinaryTreePathSum problem. Renamed tests since the implementation is no longer intended to be greedy.

--- DSA/Problems/BinaryTreePathSum.cs
+++ DSA/Problems/BinaryTreePathSum.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using DSA.Structures;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -16,9 +17,20 @@ namespace DSA.Problems
         /// <output>
         ///     Collection of node values which form the path to the sum or null if none exists
         /// </output>
-        public static List<int> GreedyAlgorithm(BinaryTreeNode<int> node, int sum)
+        public static List<int> Algorithm(BinaryTreeNode<int> node, int sum)
         {
-            return null;
+            // Time: O(n).      Linear, where n is the number of input nodes. Each node is read once to find the optimal solution.
+            //                  Could shortcut some routes when a solution has been found, but this won't affect big o time.
+            // Memory: O(n).    Linear, where n is the number of input nodes. The number of items in the optimal path may equal the number of incoming nodes.
+
+            // Store valid solutions by their route length. Overwriting existing values is fine, since their length is the only value we care about.
+            // Use the shortest route length value to get the route itself from the dict.
+            Dictionary<int, List<int>> routeLengthDict = new Dictionary<int, List<int>>();
+            int shortestRouteLength = int.MinValue;
+
+            // TODO: Exhaustively search the tree. Record all valid solutions found.
+
+            return routeLengthDict.ContainsKey(shortestRouteLength) ? routeLengthDict[shortestRouteLength] : null;
         }
     }
 
@@ -26,14 +38,14 @@ namespace DSA.Problems
     public class TestBinaryTreePathSum
     {
         [TestMethod]
-        public void GreedyAlgorithmNullRef()
+        public void BinaryTreePathSumNullRef()
         {
             // [Test 'null' for ref types]
-            Assert.IsNull(BinaryTreePathSum.GreedyAlgorithm(null, 1));
+            Assert.IsNull(BinaryTreePathSum.Algorithm(null, 1));
         }
 
         [TestMethod]
-        public void GreedyAlgorithmIntBoundaries()
+        public void BinaryTreePathSumIntBoundaries()
         {
             // [Testing int boundaries]
             // Create nodes that will cause wrapping since C# handles overflow by default this way
@@ -43,23 +55,23 @@ namespace DSA.Problems
             var overflowNodeNegative = new BinaryTreeNode<int>(int.MaxValue) { Left = new BinaryTreeNode<int>(1) };
             var overflowNodePositive = new BinaryTreeNode<int>(int.MinValue) { Left = new BinaryTreeNode<int>(-1) };
 
-            Assert.IsNull(BinaryTreePathSum.GreedyAlgorithm(overflowNodeNegative, int.MinValue));
-            Assert.IsNull(BinaryTreePathSum.GreedyAlgorithm(overflowNodePositive, int.MaxValue));
+            Assert.IsNull(BinaryTreePathSum.Algorithm(overflowNodeNegative, int.MinValue));
+            Assert.IsNull(BinaryTreePathSum.Algorithm(overflowNodePositive, int.MaxValue));
         }
 
         [TestMethod]
-        public void GreedyAlgorithmWithoutSolution()
+        public void BinaryTreePathSumWithoutSolution()
         {
             // [Test a set which contains no solution]
             /*  Goal: 2. No route.
              *      1
              */
             var nodeWithoutSolution = new BinaryTreeNode<int>(1);
-            Assert.IsNull(BinaryTreePathSum.GreedyAlgorithm(nodeWithoutSolution, 2));
+            Assert.IsNull(BinaryTreePathSum.Algorithm(nodeWithoutSolution, 2));
         }
 
         [TestMethod]
-        public void GreedyAlgorithmWithSolution()
+        public void BinaryTreePathSumWithSolution()
         {
             // [Test a set which contains a solution]
             /*  Goal: 2. Root, left.
@@ -68,14 +80,14 @@ namespace DSA.Problems
              *          0
              */
             var nodeWithSolution = new BinaryTreeNode<int>(1) { Left = new BinaryTreeNode<int>(1) };
-            var solutionResult = BinaryTreePathSum.GreedyAlgorithm(nodeWithSolution, 2);
+            var solutionResult = BinaryTreePathSum.Algorithm(nodeWithSolution, 2);
 
             Assert.IsNotNull(solutionResult);
             Assert.AreEqual(2, solutionResult.Count);
         }
 
         [TestMethod]
-        public void GreedyAlgorithmSubOptimalSolution()
+        public void BinaryTreePathSumSubOptimalSolution()
         {
             // [Test a set which contains an optimal and sub-optimal solution]
             /*  Goal: 10. Root, left, left, for a total of 3 steps. Root, right, for a total of 2 steps.
@@ -86,7 +98,7 @@ namespace DSA.Problems
             var nodeWithSubOptimalSolution = new BinaryTreeNode<int>(1) { Left = new BinaryTreeNode<int>(1), Right = new BinaryTreeNode<int>(9) };
             nodeWithSubOptimalSolution.Left.Left = new BinaryTreeNode<int>(1) { Left = new BinaryTreeNode<int>(8) };
 
-            var subOptimalSolutionResult = BinaryTreePathSum.GreedyAlgorithm(nodeWithSubOptimalSolution, 2);
+            var subOptimalSolutionResult = BinaryTreePathSum.Algorithm(nodeWithSubOptimalSolution, 2);
             Assert.IsNotNull(subOptimalSolutionResult);
             Assert.AreEqual(2, subOptimalSolutionResult.Count);
         }
