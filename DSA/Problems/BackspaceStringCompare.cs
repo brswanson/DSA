0986d618a3fba2a9352f8262efaf68b108d5db1c
Ben Swanson <swansob@gmail.com>
2018-06-09 17:05:32 -0400

    Added edge cases and shortcuts for BackspaceStringCompare.

--- DSA/Problems/BackspaceStringCompare.cs
+++ DSA/Problems/BackspaceStringCompare.cs
@@ -20,13 +20,27 @@ namespace DSA.Problems
         // Memory:  O(1). Constant. Additional memory is only used to keep track of string iterators and backspace occurences.
         public static bool BruteForce(string a, string b)
         {
+            if (IsEqualShortcut(a, b)) return true;
             if (IsInvalidParams(a, b)) return false;
+
+            // Start from the right side of both strings since backspace characters remove characters to their left
+            // Increment backspace counts when backspaces are found, decrement and ignore current character when they're not
 
             return true;
         }
 
+        private static bool IsEqualShortcut(string a, string b)
+        {
+            if (a == null && b == null) return true;
+            if (a == string.Empty && b == string.Empty) return true;
+
+            return false;
+        }
+
         private static bool IsInvalidParams(string a, string b)
         {
+            if (a == null && b != null) return true;
+            if (b == null && a != null) return true;
 
             return false;
         }
@@ -35,6 +49,21 @@ namespace DSA.Problems
     [TestClass]
     public class TestBackspaceStringCompare
     {
+        [TestMethod]
+        public void BruteForceEmptyString()
+        {
+            Assert.IsTrue(BackspaceStringCompare.BruteForce(string.Empty, string.Empty));
+        }
+
+        [TestMethod]
+        public void BruteForceNulls()
+        {
+            Assert.IsTrue(BackspaceStringCompare.BruteForce(null, null));
+
+            Assert.IsFalse(BackspaceStringCompare.BruteForce("a", null));
+            Assert.IsFalse(BackspaceStringCompare.BruteForce(null, "b"));
+        }
+
         [TestMethod]
         public void BruteForce()
         {
