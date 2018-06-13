d887dc06db6202d8f6622db8bd4c7e6c6524a50a
Ben Swanson <b.swanson@kunzleigh.com>
2018-06-13 14:31:10 -0400

    Added an iterative BFS solution with the similar performance but better memory consumption.

--- DSA/Problems/BinaryTreeDepth.cs
+++ DSA/Problems/BinaryTreeDepth.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using DSA.Structures;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -31,6 +32,38 @@ namespace DSA.Problems
             if (IsInvalid(root)) return -1;
 
             return GetMaximumNodeDepth(root);
+        }
+
+        public static int IterativeBreadthFirstSearch(BinaryTreeNode<int> root)
+        {
+            // Time:    O(n).   Linear, where n is the number of input nodes. Each node is visited once to find the maximum depth.
+            // Memory:  O(d*k). Linear, where d is the maximum depth of the tree and k is the number of leaves on each node (d*2).
+            //                  Worst case, the widest level of the tree will exist in the heap using the queue's memory space.
+
+            if (IsInvalid(root)) return -1;
+
+            var maxDepth = 0;
+            var queue = new Queue<BinaryTreeNode<int>>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                // Mark the number of items currently in the queue for this level
+                var currentLevelNodeCount = queue.Count;
+                maxDepth++;
+
+                // Dequeue all items for the current level and enqueue their children
+                for (var i = 0; i < currentLevelNodeCount; i++)
+                {
+                    var currentNode = queue.Dequeue();
+                    if (currentNode == null) { continue; }
+
+                    if (currentNode.Left != null) queue.Enqueue(currentNode.Left);
+                    if (currentNode.Right != null) queue.Enqueue(currentNode.Right);
+                }
+            }
+
+            return maxDepth;
         }
 
         private static int GetMaximumNodeDepth(BinaryTreeNode<int> root, int currentDepth = 0)
@@ -98,6 +131,60 @@ namespace DSA.Problems
 
             var expected = 2;
             var actual = BinaryTreeDepth.RecursiveDepthFirstSearch(testNode);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BinaryTreeDepthIterativeNullRef()
+        {
+            Assert.AreEqual(-1, BinaryTreeDepth.IterativeBreadthFirstSearch(null));
+        }
+
+        [TestMethod]
+        public void BinaryTreeDepthIterativePartialLeaves()
+        {
+            //           3
+            //         /   \
+            //        9     20
+            //       /       \
+            //      15        7
+            //                 \
+            //                  12 
+
+            var testNode = new BinaryTreeNode<int>(3)
+            {
+                Left = new BinaryTreeNode<int>(9),
+                Right = new BinaryTreeNode<int>(20)
+            };
+            testNode.Left.Left = new BinaryTreeNode<int>(15);
+
+            testNode.Right.Right = new BinaryTreeNode<int>(7)
+            {
+                Right = new BinaryTreeNode<int>(12)
+            };
+
+            var expected = 4;
+            var actual = BinaryTreeDepth.IterativeBreadthFirstSearch(testNode);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BinaryTreeDepthIterativeFullLeaves()
+        {
+            //           3
+            //         /   \
+            //        9     20
+
+            var testNode = new BinaryTreeNode<int>(3)
+            {
+                Left = new BinaryTreeNode<int>(9),
+                Right = new BinaryTreeNode<int>(20)
+            };
+
+            var expected = 2;
+            var actual = BinaryTreeDepth.IterativeBreadthFirstSearch(testNode);
 
             Assert.AreEqual(expected, actual);
         }
