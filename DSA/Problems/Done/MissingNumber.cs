720c6f18324a52a30567b410504f90c6f71cc309
Ben Swanson <b.swanson@kunzleigh.com>
2018-06-18 13:14:17 -0400

    Refactored the MissingNumber problem to demonstrate the differences between the two methods: success on unsorted arrays of numbers.

--- DSA/Problems/Done/MissingNumber.cs
+++ DSA/Problems/Done/MissingNumber.cs
@@ -3,25 +3,42 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace DSA.Problems.Done
 {
     /// <summary>
-    ///     Given a contiguous sequence of numbers in which each number repeats thrice, there is exactly one missing number.
+    ///     Given a contiguous sequence of sorted numbers in which each number repeats thrice, there is exactly one missing number.
     ///     Find the missing number.
     /// </summary>
     /// <input>
     ///     [1,1,1,2,2,3,3,3]
     /// </input>
+    /// <mutation>
+    ///     Numbers are not guaranteed to be sorted.
+    /// </mutation>
     /// <output>
     ///     2
     /// </output>
     public class MissingNumber
     {
-        public static int? CountFrequency(int repetitionCount, int[] input)
+        public static int? Bitwise(int repetitionCount, int[] input)
         {
             // Time:    O(n).   Linear, iterates over each number in the passed in array.
+            // Memory:  O(1).   A constant amount of memory is used to keep track of the current XOR'd value.
+
+            // Checking edge cases
+            if (IsInvalid(repetitionCount, input)) return null;
+
+            int output = 0;
+
+            foreach (var num in input) output = output ^ num;
+
+            return output;
+        }
+
+        public static int? CountFrequency(int repetitionCount, int[] input)
+        {
+            // Time:    O(n).   Linear, iterates over each number in the passed in array. Numbers must be ordered.
             // Memory:  O(1).   A constant amount of memory is used to keep track of the current character, its count, and to return the output number.
 
             // Checking edge cases
-            if (input == null) return null;
-            if (repetitionCount <= 0) return null;
+            if (IsInvalid(repetitionCount, input)) return null;
 
             int? output = null;
             var currentNumCount = 0;
@@ -50,20 +67,12 @@ namespace DSA.Problems.Done
             return output;
         }
 
-        public static int? Bitwise(int repetitionCount, int[] input)
+        private static bool IsInvalid(int repetitionCount, int[] input)
         {
-            // Time:    O(n).   Linear, iterates over each number in the passed in array.
-            // Memory:  O(1).   A constant amount of memory is used to keep track of the current XOR'd value.
+            if (input == null) return true;
+            if (repetitionCount <= 0) return true;
 
-            // Checking edge cases
-            if (input == null) return null;
-            if (repetitionCount <= 0) return null;
-
-            int xorValue = 0;
-
-            foreach (var num in input) xorValue = xorValue ^ num;
-
-            return xorValue;
+            return false;
         }
     }
 
@@ -71,7 +80,31 @@ namespace DSA.Problems.Done
     public class TestMissingNumber
     {
         [TestMethod]
-        public void BruteForce()
+        public void BitwiseSorted()
+        {
+            const int repetitionCount = 3;
+            var input = new[] { 1, 1, 1, 2, 2, 3, 3, 3 };
+            const int expected = 2;
+
+            var actual = MissingNumber.Bitwise(repetitionCount, input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BitwiseUnsorted()
+        {
+            const int repetitionCount = 3;
+            var input = new[] { 1, 2, 3, 1, 2, 3, 1, 3 };
+            const int expected = 2;
+
+            var actual = MissingNumber.Bitwise(repetitionCount, input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CountFrequencySorted()
         {
             const int repetitionCount = 3;
             var input = new[] { 1, 1, 1, 2, 2, 3, 3, 3 };
@@ -83,15 +116,17 @@ namespace DSA.Problems.Done
         }
 
         [TestMethod]
-        public void Bitwise()
+        public void CountFrequencyUnsorted()
         {
             const int repetitionCount = 3;
-            var input = new[] { 1, 1, 1, 2, 2, 3, 3, 3 };
+            var input = new[] { 1, 2, 3, 1, 2, 3, 1, 3 };
             const int expected = 2;
 
-            var actual = MissingNumber.Bitwise(repetitionCount, input);
+            var actual = MissingNumber.CountFrequency(repetitionCount, input);
 
-            Assert.AreEqual(expected, actual);
+            // Testing 'are not equal' because this algorithm only works when the numbers are sorted by value
+            // It should fail on an unsorted array of numbers
+            Assert.AreNotEqual(expected, actual);
         }
     }
 }
