47b9bef42c40e1d780baae00878a3c4ea05d2e22
Ben Swanson <swansob@gmail.com>
2018-06-26 14:34:06 -0400

    Adding edge case checks.

--- DSA/Sorting/QuickSort.cs
+++ DSA/Sorting/QuickSort.cs
@@ -9,8 +9,11 @@ namespace DSA.Sorting
             // Time:    O(nlog(n))  Array is halved log(n) times. Each element in the array is swapped once for a total of n.    
             // Memory:  O(log(n)).  The same array is used on each recursive call, but because of the recursive implemenation there will be up to log(n) stack frames.
 
+            // Edge cases
+            if (IsInvalidInput(input)) return input;
+
             // TODO: Implement this
-            return null;
+            return input;
         }
 
         public static int[] Iterative(int[] input)
@@ -18,9 +21,14 @@ namespace DSA.Sorting
             // Time:    O(nlog(n))  Array is halved log(n) times. Each element in the array is swapped once for a total of n.    
             // Memory:  O(log(n)).  The same array is used during the loop, but an auxilary stack of log(n) entries is needed to keep track of start/end of each array partition.
 
+            // Edge cases
+            if (IsInvalidInput(input)) return input;
+
             // TODO: Implement this
             return null;
         }
+
+        private static bool IsInvalidInput(int[] input) => input == null || input.Length <= 1;
     }
 
     [TestClass]
@@ -38,10 +46,32 @@ namespace DSA.Sorting
         }
 
         [TestMethod]
+        public void RecursiveNullRef()
+        {
+            int[] input = null;
+            int[] expected = null;
+
+            var actual = QuickSort.Recursive(input);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
         public void Iterative()
         {
             var input = new[] { 7, 3, 1, 9, 8, 2, 4, 6, 5, 0 };
             var expected = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var actual = QuickSort.Iterative(input);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IterativeNullRef()
+        {
+            int[] input = null;
+            int[] expected = null;
 
             var actual = QuickSort.Iterative(input);
 
