08b5672b3014c2e0b86102b74b03a0af9929fbf2
Ben Swanson <b.swanson@kunzleigh.com>
2018-06-28 10:14:49 -0400

    Added some additional unit tests to MergeSort. Rewrote the comments to better describe the problem.

--- DSA/Sorting/MergeSort.cs
+++ DSA/Sorting/MergeSort.cs
@@ -7,14 +7,13 @@ namespace DSA.Sorting
     {
         public static int[] Recursive(int[] input)
         {
-            // Time:    O(nlog(n)).   Arrays are halved on each recursive call, leaving logn total merges. Sorting all the halved arrays takes n time.
+            // Time:    O(nlog(n)). Arrays are halved on each recursive call, leaving log(n) total merges.
+            //                      Sorting each halved array takes up to n time for a total of n log(n).
             // Memory:  O(n).       New arrays are created and their memory is allocated at each recursive step.
             //                      This additional memory reduces down to linear space in big O notation though.
 
             // Checking edge cases
-            if (input == null) return null;
-
-            if (input.Length <= 1) return input;
+            if (IsInvalidInput(input)) return input;
 
             var mid = input.Length / 2;
             // Trade off here of using Skip/Take instead of Array.Copy()
@@ -26,12 +25,18 @@ namespace DSA.Sorting
 
         public static int[] Iterative(int[] input)
         {
-            // Time:    
-            // Memory:  
+            // Time:    O(nlog(n)). Arrays are halved log(n) times. Sorting each halved array takes up to n time for a total of n log(n).
+            // Memory:  O(n).       New arrays are created and their memory is allocated for each partitioning of the array.
+            //                      This additional memory reduces down to linear space in big O notation though.
+
+            // Checking edge cases
+            if (IsInvalidInput(input)) return input;
 
             // TODO: Implement this
             return null;
         }
+
+        private static bool IsInvalidInput(int[] input) => input == null || input.Length <= 1;
 
         private static int[] Merge(int[] arrayA, int[] arrayB)
         {
@@ -94,10 +99,32 @@ namespace DSA.Sorting
         }
 
         [TestMethod]
+        public void RecursiveNullRef()
+        {
+            int[] input = null;
+            int[] expected = null;
+
+            var actual = MergeSort.Recursive(input);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
         public void Iterative()
         {
             var input = new[] { 7, 3, 1, 9, 8, 2, 4, 6, 5, 0 };
             var expected = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var actual = MergeSort.Iterative(input);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IterativeNullRef()
+        {
+            int[] input = null;
+            int[] expected = null;
 
             var actual = MergeSort.Iterative(input);
 
